6cd3dc87f6198be36b7d0cd573938b1f3370adbf
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2018-11-23 11:44:27 +0200

    add_add

---
+++ app/src/main/java/farkasnorbert/sapientia/ms/androidprojekt/add_ad.java
@@ -0,0 +1,44 @@
+package farkasnorbert.sapientia.ms.androidprojekt;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class add_ad extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_add:
+                    mTextMessage.setText(R.string.add);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.settings);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ad);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
