fff92aa1064de1d37ed59f2131b86260c5dae8af
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2018-11-23 11:11:14 +0200

    Login ui

--- app/src/main/java/farkasnorbert/sapientia/ms/androidprojekt/MainActivity.java
+++ app/src/main/java/farkasnorbert/sapientia/ms/androidprojekt/MainActivity.java
@@ -1,7 +1,12 @@
 package farkasnorbert.sapientia.ms.androidprojekt;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginb = findViewById(R.id.login_button);
+        loginb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView phone = findViewById(R.id.phone_number);
+                Intent send = new Intent(MainActivity.this,login_activity.class);
+                send.putExtra("Phone",phone.toString());
+                startActivity(send);
+            }
+        });
     }
+
+
 }
