3bc4cafe26767b5477591c952d2938438f906c53
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2018-12-04 08:20:59 +0200

    add_add image put in firebase storage working

---
+++ app/src/main/java/farkasnorbert/sapientia/ms/androidprojekt/ads.java
@@ -0,0 +1,40 @@
+package farkasnorbert.sapientia.ms.androidprojekt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class ads extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_add:
+                    startActivity(new Intent(ads.this, add_ad.class));
+                    return true;
+                case R.id.navigation_settings:
+                    //startActivity(new Intent(ads.this, login_activity.class));
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ads);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
