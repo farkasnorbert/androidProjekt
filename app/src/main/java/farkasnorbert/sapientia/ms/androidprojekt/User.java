0c9fa852e53ba6174697b273c4eee313de6f49ed
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2018-12-07 23:43:46 +0200

    log in and register working

---
+++ app/src/main/java/farkasnorbert/sapientia/ms/androidprojekt/User.java
@@ -0,0 +1,46 @@
+package farkasnorbert.sapientia.ms.androidprojekt;
+
+public class User {
+    private String fName;
+    private String lName;
+    private String email;
+    private String pPicture;
+
+    public User(String fName, String lName, String email, String pPicture) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getpPicture() {
+        return pPicture;
+    }
+
+    public void setpPicture(String pPicture) {
+        this.pPicture = pPicture;
+    }
+}
